27c9c3a8be4f96450a22f89f865e0a60256cbad2
Tanrada <tanrada_wichian@cmu.ac.th>
2024-09-26 13:49:28 +0700

    2.1-2.2

--- src/main/java/se331/rest/entity/Participant.java
+++ src/main/java/se331/rest/entity/Participant.java
@@ -1,10 +1,22 @@
 package se331.rest.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
 import java.util.List;
+import se331.rest.entity.Event;
 
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Long id;
     String name;
     String telNo;
+    @ManyToMany
     List<Event> eventHistory;
 }
