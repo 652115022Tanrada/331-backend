dff43c54b59dc59bc16369f5ed6e9c3e1a223d69
Tanrada <tanrada_wichian@cmu.ac.th>
2024-10-03 16:10:20 +0700

    6.2 update

--- src/main/java/se331/rest/entity/OrganizerOwnEventsDTO.java
+++ src/main/java/se331/rest/entity/OrganizerOwnEventsDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrganizerOwnEventDTO {
+public class OrganizerOwnEventsDTO {
     Long id;
     String category;
     String title;
