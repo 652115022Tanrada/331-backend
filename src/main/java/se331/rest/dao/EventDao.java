eecb660e046e82f0c62fe980836c07078ec738f4
Tanrada <tanrada_wichian@cmu.ac.th>
2024-10-16 09:29:49 +0700

    1

--- src/main/java/se331/rest/dao/EventDao.java
+++ src/main/java/se331/rest/dao/EventDao.java
@@ -2,10 +2,12 @@ package se331.rest.dao;
 
 import org.springframework.data.domain.Page;
 import se331.rest.entity.Event;
+import org.springframework.data.domain.Pageable;
 
 public interface EventDao {
     Integer getEventSize();
     Page<Event> getEvents(Integer pageSize, Integer page);
     Event getEvent(Long id);
     Event save(Event event);
+    Page<Event> getEvents(String name, Pageable page);
 }
--- src/main/java/se331/rest/repository/EventRepository.java
+++ src/main/java/se331/rest/repository/EventRepository.java
@@ -1,10 +1,12 @@
 package se331.rest.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se331.rest.entity.Event;
-
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAll();
+    Page<Event> findByTitle(String title, Pageable pageRequest);
 }
